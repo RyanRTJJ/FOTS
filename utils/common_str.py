75fa5b95973b07e4e6a0b4f32a9bb335eeb91011
RyanRTJJ <50686301+RyanRTJJ@users.noreply.github.com>
2019-05-27 11:00:53 +0800

    Added new string

--- utils/common_str.py
+++ utils/common_str.py
@@ -3,3 +3,4 @@ alphabet = 'qwertyuiopasdfghjklzxcvbnm' + 'QWERTYUIOPASDFGHJKLZXCVBNM'
 alphabet_and_number = alphabet + number
 custom_1 = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-.'
 number_and_dot = number + '.'
+ryan_1 = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-.,!@#$%^&*()\'/:<>+=\\\"?[]_;|~ '
